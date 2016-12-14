138a49acf5b99f17e9e3a8be08952094294e3d81
VanessaMahua <vanessa.macedo@ucsp.edu.pe>
2016-12-14 16:57:15 -0500

    Update P_9.cpp

--- P_9.cpp
+++ P_9.cpp
@@ -50,3 +50,95 @@ int division(int a , int b)
      cout <<"Ingrese (0)\n suma(1)resta \n (2)multiplicacion \n (3)division:\n";cin>>opc;
     }
  }
+#include <iostream>
+using namespace std;
+class cuenta
+{
+    //sobrentiende privados
+    string nro;
+    float saldo;
+    public:
+    cuenta (string, float);
+    void setnro(string);
+    void setsaldo(float);
+    string getnro();
+    float getsaldo();
+    void depositar(float);
+    void retirar(float);
+};
+cuenta :: cuenta(string n, float s)
+{
+    nro=n;
+    saldo=s;
+
+}
+void cuenta:: setnro(string n)
+{
+    nro=n;
+}
+void cuenta:: setsaldo(float s)
+{
+    saldo=s;
+}
+float cuenta ::  getsaldo()
+{
+    return saldo;
+}
+void cuenta:: depositar(float x)
+{
+    int total=x+getsaldo();
+    setsaldo(total);
+}
+void cuenta:: retirar(float x)
+{
+    int total =getsaldo()-x;
+    setsaldo(total);
+}
+int main()
+{
+
+
+    float saldo ,dep,ret ;
+    int opc;
+    cout<<("Ingrese monto de saldo: ");cin>>saldo;
+    cuenta c1("0001", saldo );
+    //cout<<"saldo= "<<c1.getsaldo()<<endl;
+    //c1.depositar(2000);
+    //cout<<"saldo= "<<c1.getsaldo()<<endl;
+    //c1.retirar(1000);
+    //cout<<"saldo= "<<c1.getsaldo()<<endl;
+
+
+    cout <<"\n  (0) depositar\n (1) mostrar saldo\n (2)retirar \n (3)salir:\n";cin>>opc;
+
+     while (true)
+     {
+
+     if(opc==0)
+        {
+            cout<<("Ingrese monto a depositar: ");cin>>dep;
+            c1.depositar(dep);
+
+        }
+    else if (opc==1)
+     {
+                    cout<<c1.getsaldo();
+     }
+
+     else if (opc==2)
+     {
+            cout<<("Ingrese monto a retirar: ");cin>>ret;
+
+            c1.retirar(ret);
+     }
+
+
+    else
+        break;
+     cout <<" \n (0) depositar\n (1) mostrar saldo\n (2)retirar \n (3)salir:\n";cin>>opc;
+    }
+
+
+    return 0;
+}
+
